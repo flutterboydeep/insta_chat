12699fd22fb9aa1e3cd318218e80a542d08c55a8
Your Name <you@example.com>
2024-04-06 14:02:03 +0530

    Auth ui part

--- lib/xfirebse/firebaseAuth.dart
+++ lib/xfirebse/firebaseAuth.dart
@@ -1,14 +1,9 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-// class AuthMethods {
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
 // Future<String> signUpUsingMail()async{
-  
 
 // }
-
-
-
-
-// }
+}
